290ea20f6c9aabe857298b445048a36acaae5ec7
Robert Venezia <rvenezia@cs.hartford.edu>
2018-06-09 19:46:06 -0400

    fix attempt #1

--- main/java/com/venrob/robsstuff/proxy/ClientProxy.java
+++ main/java/com/venrob/robsstuff/proxy/ClientProxy.java
@@ -1,16 +1,30 @@
 package com.venrob.robsstuff.proxy;
 
+import com.venrob.robsstuff.init.ModItems;
+import com.venrob.robsstuff.util.IHasModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Objects;
 
+@Mod.EventBusSubscriber
 public class ClientProxy extends CommonProxy {
 
     @Override
     public void registerItemRenderer(Item item, int meta, String id){
         ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()),id));
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event){
+        for(Item item : ModItems.ITEMS){
+            if(item instanceof IHasModel){
+                ((IHasModel)item).registerModels();
+            }
+        }
     }
 }
--- main/java/com/venrob/robsstuff/util/handlers/RegistryHandler.java
+++ main/java/com/venrob/robsstuff/util/handlers/RegistryHandler.java
@@ -1,14 +1,10 @@
 package com.venrob.robsstuff.util.handlers;
 
 import com.venrob.robsstuff.init.ModItems;
-import com.venrob.robsstuff.util.IHasModel;
 import net.minecraft.item.Item;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class RegistryHandler {
@@ -18,12 +14,4 @@ public class RegistryHandler {
         event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
     }
 
-    @SubscribeEvent @SideOnly(Side.CLIENT)
-    public static void onModelRegister(ModelRegistryEvent event){
-        for(Item item : ModItems.ITEMS){
-            if(item instanceof IHasModel){
-                ((IHasModel)item).registerModels();
-            }
-        }
-    }
 }
